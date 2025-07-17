9b71155db1e98e91f868bedc3a5759bce35c8574
Agustin Malugani <agusmalugani97@gmail.com>
2025-07-16 21:24:15 -0300

    refactor: reorganizacion de codigo

--- src/app/api/asistencias/registro-qr/route.ts
+++ src/app/api/asistencias/registro-qr/route.ts
@@ -3,12 +3,12 @@ import { authMiddleware } from '@/middlewares/auth.middleware';
 import { registrarAsistenciaDesdeQR } from '@/services/asistenciaService';
 
 export async function GET(req: NextRequest) {
+  try{
   //VERIFICO QUE ESTEEL USUARIO LOGUEADO
   const { user } = await authMiddleware(req); 
 if(!user){
   return NextResponse.json({error:"El usuario no esta logueado"},{status:400})
 }
-
   //AGARRO LA QUERY DE LA URL
   const { searchParams } = new URL(req.url)
   const tokenAsistencia = searchParams.get('tokenAsistencia')
@@ -18,7 +18,6 @@ if(!user){
   }
 
 
-  try {
     //LE MANDO EL TOKEN DE LA ASISTENCIA Y EL USUARIO LOGUEADO
     const registro = await registrarAsistenciaDesdeQR(tokenAsistencia, user);
    
