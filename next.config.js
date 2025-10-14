cdb6a9a9272ff66013415ca173a34b65f47f9538
esspíndola <franespindola71@gmail.com>
2025-10-14 18:15:06 -0300

    hotfix mjs to js

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // evita errores en desarrollo
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: {},
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'www.aesthetics-blog.com' },
+      { protocol: 'https', hostname: 'i.pinimg.com' },
+      { protocol: 'https', hostname: 'vitruve.fit' },
+      { protocol: 'https', hostname: 'boxlifemagazine.com' },
+      { protocol: 'https', hostname: 'fitcron.com' },
+      { protocol: 'https', hostname: 'menspower.nl' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+    ],
+  },
+};
+
+// Exporta la configuración combinada con PWA
+module.exports = withPWA(nextConfig);
